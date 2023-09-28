9012489063dd3b2373f91517f1766687dd614557
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-28 20:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+// Shallow copy of the players array
+const team = [...players]; // This creates a new array with the same elements
+
+// Deep copy of the person object
+const cap1 = { ...person }; // This creates a new object with the same properties and values
+
+// Modifying the copied objects
+team.push("Ram"); // Modifying team should not affect players
+cap1.name = "Jane";     // Modifying cap1 should not affect person
+
+
 window.players = players;
 window.person = person;
 window.team = team;
